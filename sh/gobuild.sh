c8c1078210ce5591d3f1bc3fe3dcf57c7a6d29f7
kos <vbz007@hotmail.com>
2023-04-25 10:26:05 +0800

    fit windows cross build on linux.

--- sh/gobuild.sh
+++ sh/gobuild.sh
@@ -14,8 +14,13 @@ echo "build for linux_amd64..."
 CGO_ENABLED=1 CC=gcc GOOS=linux GOARCH=amd64 go build -tags=nomsgpack,noproto,novalidator,fts5 -ldflags '-s -w'
 elif [ "$1" == "4" ]; then
 echo "build for win_amd64..."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -tags=nomsgpack,noproto,novalidator,fts5 -ldflags '-s -w'
+#x86_64-w64-mingw32-cross.tgz
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -tags=nomsgpack,noproto,novalidator,fts5 -ldflags '-s -w'
 elif [ "$1" == "5" ]; then
+echo "build for win_x86..."
+#i686-w64-mingw32-cross.tgz
+CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -tags=nomsgpack,noproto,novalidator,fts5 -ldflags '-s -w'
+elif [ "$1" == "6" ]; then
 echo "build for darwin_amd64..."
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -tags=nomsgpack,noproto,novalidator -ldflags '-s -w'
 else
